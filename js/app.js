b8e59ac32a0009e2bf32f75393e5408bfbf6a50d
Davi Andion <andiondavimelo@gmail.com>
2023-12-15 14:33:51 -0300

    adding a return confirmation

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ function changeStatus(id) {
     let chosenGame = document.getElementById(`game-${id}`);
     let image = chosenGame.querySelector('.dashboard__item__img');
     let button = chosenGame.querySelector('.dashboard__item__button');
+    let gameName = chosenGame.querySelector('.dashboard__item__name');
 
     if (image.classList.contains('dashboard__item__img--rented')) {
-        image.classList.remove('dashboard__item__img--rented');
-        button.classList.remove('dashboard__item__button--return');
-        button.textContent = 'Rent';
+        if (confirm(`Are you sure you want to return the game ${gameName.textContent}?`)) {
+            image.classList.remove('dashboard__item__img--rented');
+            button.classList.remove('dashboard__item__button--return');
+            button.textContent = 'Rent';
+        }
     } else {
         image.classList.add('dashboard__item__img--rented');
         button.classList.add('dashboard__item__button--return');
